93615e2eeadf8758be4c8fa28272eea9ee49982e
cq522138 <tuanlinhvu1991@gmail.com>
2018-11-30 15:36:17 +0700

    add new file

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vutuanlinh
+ * Date: 11/30/18
+ * Time: 3:25 PM
+ */
+
+return [
+    'title' => 'Message was written by english.',
+];
---
+++ resources/lang/vi/messages.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vutuanlinh
+ * Date: 11/30/18
+ * Time: 3:26 PM
+ */
+
+return [
+    'title' => 'Tin nhắn được viết bằng tiếng việt.',
+];
